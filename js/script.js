0d15494819a55d68754966db468f24aa6c4f32a1
Tomasz Sypko <tsypko@gmail.com>
2020-06-29 23:03:37 +0200

    add & remove tasks added

--- js/script.js
+++ js/script.js
@@ -13,23 +13,51 @@
     },
   ];
 
-  const doneButton = call(".js-doneButton");
-  const deleteButton = call(".js-deleteButton");
-  const taskFrom = call(".js-taskForm");
+  const doneTaskButton = call(".js-doneButton");
+  const deleteTaskButton = call(".js-deleteButton");
+  const addTaskButton = call(".js-addButton");
 
   const render = () => {
     let htmlString = "";
     for (const task of tasks)
       htmlString += `<li class="section__item">
     <button class="section__button section__button--doneButton js-doneButton"></button>
-    <p class="section__paragraph js-task">${task.content}</p>
+    <p class="section__paragraph ${
+      task.done ? "section__paragraph--done" : ""
+    } js-task">${task.content}</p>
     <button class="section__button section__button--deleteButton js-deleteButton"></button>
     </li>`;
     call(".js-taskList").innerHTML = htmlString;
+
+    const removeButtons = callAll(".js-deleteButton");
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => removeTask(index));
+    });
+  };
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+  };
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const taskSubmit = (event) => {
+    event.preventDefault();
+    const newTaskContent = call(".js-taskNewTaskInput").value.trim();
+    if (newTaskContent === "") return;
+    else addNewTask(newTaskContent);
+    render();
   };
 
   const init = () => {
     render();
+    const taskForm = call(".js-taskForm");
+    taskForm.addEventListener("submit", taskSubmit);
+ 
   };
 
   init();
